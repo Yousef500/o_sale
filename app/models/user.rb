0d755112ed4bc5e2cafa7860b47e4b5aff70bc71
Yousef500 <Kazuya14725@gmail.com>
2021-04-24 22:02:01 +0300

    Finished lesson number 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, presence: true
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
   has_secure_password
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
 end
